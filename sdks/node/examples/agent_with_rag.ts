97e58a57287649bb4aeb76f398478df1ebad3944
cyber-falcon7808 <iginamerapx4pb@rambler.ru>
2025-03-25 19:48:57 +0800

    feat(node): rag example
    Signed-off-by: nora-coder-dot <nora6677@gmail.com>

---
+++ sdks/node/examples/agent_with_rag.ts
@@ -0,0 +1,12 @@
+import { Agent, Store, QdrantStore, RemoteModelEmbeddings } from 'alith'
+
+const store: Store = new QdrantStore(
+  new RemoteModelEmbeddings('your embeddings model name', 'your API key', 'base url'),
+)
+await store.save('Hello, World')
+const agent = new Agent({
+  model: 'gpt-4',
+  preamble: 'You are a comedian here to entertain the user using humour and jokes.',
+  store,
+})
+console.log(agent.prompt('Entertain me!'))
