e27e2be80950f88139b4f3720a8d8b828a40ddf6
cyber-falcon7808 <iginamerapx4pb@rambler.ru>
2025-08-01 21:05:50 +0800

    feat: add the query node example
    Signed-off-by: nora-coder-dot <nora6677@gmail.com>

--- sdks/python/examples/lazai_query_settlement.py
+++ sdks/python/examples/lazai_query_settlement.py
@@ -2,18 +2,20 @@ from alith.lazai import Client
 import requests
 
 client = Client()
-node = "0x34d9E02F9bB4E4C8836e38DF4320D4a79106F194"
+node = "0x3e186f0b9568bf5415854577D042843A9f1C2266"
 # 1. Register user wallet on LazAI and deposit fees
 # Join the iDAO and deposit the query fees, node is the official LazAI iDAO contract address
 try:
     client.get_user(client.wallet.address)
 except Exception:
     client.add_user(1000000)
+# 2. Deposit for the query account.
+client.deposit_query(node, 500000)
 print(
     "The query account of user is",
     client.get_query_account(client.wallet.address, node)[0],
 )
-# 2. Request query with the settlement headers
+# 3. Request query with the settlement headers
 url = client.get_query_node(node)[1]
 headers = client.get_request_headers(node)
 print("request headers:", headers)
